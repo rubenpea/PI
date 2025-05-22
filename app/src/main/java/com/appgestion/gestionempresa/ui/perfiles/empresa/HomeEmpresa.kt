1df3ccfdd52088277d9085be39e61efc14b07ce4
ruben <rubon94@hotmail.com>
2025-05-22 12:14:23 +0200

    Limpio archivos y carpetas ignore

--- app/src/main/java/com/appgestion/gestionempresa/ui/perfiles/empresa/HomeEmpresa.kt
+++ app/src/main/java/com/appgestion/gestionempresa/ui/perfiles/empresa/HomeEmpresa.kt
@@ -1,12 +1,22 @@
 package com.appgestion.gestionempresa.ui.perfiles.empresa
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 
 @Composable
 fun HomeScreenEmpresa(){
+    Box(
+        Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
     Text(
-        text = "Estamos dentro"
+        text = "Home Empresa"
     )
+
+    }
 }
--- app/src/main/java/com/appgestion/gestionempresa/ui/perfiles/trabajador/HomeTrabajador.kt
+++ app/src/main/java/com/appgestion/gestionempresa/ui/perfiles/trabajador/HomeTrabajador.kt
@@ -1,10 +1,18 @@
 package com.appgestion.gestionempresa.ui.perfiles.trabajador
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun HomeScreen(){
-
-    Text(text = "Home Trabajador")
-}
+    Box(
+        Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Home Trabajador")
+    }
+    }
